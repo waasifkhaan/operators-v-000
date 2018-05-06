93b4161ae07bbe9cdbe75f5fadceb525c436effa
Wasif Khan <waasif.khaan@gmail.com>
2018-05-06 20:54:56 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,14 @@
 def unsafe?(speed)
   if speed < 40 || speed > 60 
-    false
-  else 
     true
+  else 
+    false
 end
 
 
 
 def not_safe?(speed)
-	
+speed < 40 || speed > 60 ? true : false	
 end
 	
 
